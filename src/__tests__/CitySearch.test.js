e63f21b45024d4bcfe2d77083eaa3885c2774f04
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-07-29 12:39:20 +0200

    Scenario03

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -52,4 +52,12 @@ test('suggestion list match the query when changed', () => {
     expect(CitySearchWrapper.state("suggestions")).toEqual(filteredLocations);
   });
 
+test("selecting a suggestion should change query state", () => {
+    CitySearchWrapper.setState({
+      query: 'Berlin'  });
+    const suggestions = CitySearchWrapper.state('suggestions');
+    CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+    expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
+  });
+
 });
