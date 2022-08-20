8db2f51db1db0cddcc5a945b74e1836588eb7047
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-08-20 17:10:12 +0200

    Updated04 APPjs

--- src/Alert.js
+++ src/Alert.js
@@ -44,4 +44,11 @@ class WarningAlert extends Alert {
   }
 }
 
-export { InfoAlert, ErrorAlert, WarningAlert };
+class OfflineAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = "orange";
+  }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert, OfflineAlert };
