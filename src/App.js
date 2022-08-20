66bcdb816aba144933dacb230bf38484a9a12821
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-08-20 16:36:14 +0200

    updated Appjs

--- src/App.js
+++ src/App.js
@@ -1,80 +1,113 @@
-import React, { Component } from 'react';
-import './App.css';
-import CitySearch from './CitySearch';
-import EventList from './EventList';
-import NumberOfEvents from './NumberOfEvents';
-import WelcomeScreen from './WelcomeScreen';
-import { getEvents, extractLocations, checkToken, getAccessToken } from
-'./api';
-
-
+import "./nprogress.css";
+import "./App.css";
+import React, { Component } from "react";
+import NumberOfEvents from "./NumberOfEvents";
+import EventList from "./EventList";
+import CitySearch from "./CitySearch";
+import WelcomeScreen from "./WelcomeScreen";
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
+import { OfflineAlert } from "./Alert";
 
 class App extends Component {
-
   state = {
-      events: [],
-      locations: [],
-      locationSelected: 'all',
-      numberOfEvents: 32,
-      showWelcomeScreen: undefined
-  }
+    events: [],
+    locations: [],
+    currentLocation: "all",
+    numberOfEvents: 15,
+    showWelcomeScreen: undefined,
+    isOnline: navigator.onLine,
+  };
 
   async componentDidMount() {
     this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false :
-    true;
+    window.addEventListener("offline", (e) => {
+      this.setState({ isOnline: false });
+    });
+    window.addEventListener("online", (e) => {
+      this.setState({ isOnline: true });
+    });
+
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid =
+      !window.location.href.startsWith("http://localhost") &&
+      !(accessToken && !navigator.onLine) &&
+      (await checkToken(accessToken)).error
+        ? false
+        : true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
-this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-if ((code || isTokenValid) && this.mounted) {
-getEvents().then((events) => {
-if (this.mounted) {
-this.setState({ events, locations: extractLocations(events) });
-}
-});
-}
-}
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events),
+          });
+        }
+      });
+    }
+  }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.mounted = false;
   }
-  
-  updateEvents = (location) => {
+
+  updateEvents = (location, eventCount) => {
+    !location
+      ? (location = this.state.currentLocation)
+      : this.setState({ currentLocation: location });
+    !eventCount
+      ? (eventCount = this.state.numberOfEvents)
+      : this.setState({ numberOfEvents: eventCount });
     getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
       this.setState({
-        events: locationEvents
+        events: locationEvents.slice(0, eventCount),
       });
     });
-  }
+  };
 
-render() {
-  if (this.state.showWelcomeScreen === undefined) return <div className="App" />;
-  const { locations, numberOfEvents } = this.state;
-  return (
-      <div className="App">
-          <CitySearch 
-              locations={this.state.locations}  
-              updateEvents={this.updateEvents} />
-          <NumberOfEvents 
-              events={this.state.events}
-              updateEvents={this.updateEvents}/>
-          <EventList 
-              events={this.state.events}/>
-                      <WelcomeScreen
+  render() {
+    const { showWelcomeScreen } = this.state;
+
+    if (showWelcomeScreen === undefined) {
+      return <div className="App" />;
+    } else if (showWelcomeScreen === true) {
+      return (
+        <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => {
             getAccessToken();
           }}
-        />        
-      </div>
-  );
+        />
+      );
+    } else {
+      return (
+        <div className="App">
+          <div className="offline_alert_container">
+            {!this.state.isOnline && (
+              <OfflineAlert
+                style={{ top: 0 }}
+                text={
+                  "You are offline. An updated list will be loaded when you are back online."
+                }
+              />
+            )}
+          </div>
+          <CitySearch
+            updateEvents={this.updateEvents}
+            locations={this.state.locations}
+          />{" "}
+          <NumberOfEvents updateEvents={this.updateEvents} />
+          <EventList events={this.state.events} />
+        </div>
+      );
+    }
+  }
 }
-}
-
-
 
 export default App;
