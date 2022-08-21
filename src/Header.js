727cdeb7561364d2acd87cbdafc1e3691d00944d
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-08-21 17:14:40 +0200

    added Header

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Container, Navbar} from 'react-bootstrap'
+import {BsFillPeopleFill} from 'react-icons/bs'
+
+function Header() {
+  return (
+    <Navbar>
+    <Container>
+      <div className="header">
+        <BsFillPeopleFill />{' '}
+          Meetup App - Meet other people while learning what you love!
+      </div>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default Header
+
