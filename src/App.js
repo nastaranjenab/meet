5f6f451e8a528f8119dd3a2828fa20969ff75972
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-08-21 17:08:37 +0200

    added chart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from "react";
-import "./App.css";
-import NumberOfEvents from "./NumberOfEvents";
-import EventList from "./EventList";
-import CitySearch from "./CitySearch";
-import WelcomeScreen from "./WelcomeScreen";
-import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
-import { OfflineAlert } from "./Alert";
-
-
+import React, { Component } from 'react';
+import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+import './nprogress.css';
+import { Container, Row, Col } from 'react-bootstrap/'
+import Header from './Header';
+import { OfflineAlert } from './Alert';
+import WelcomeScreen from './WelcomeScreen';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import { EventGenre } from './EventGenre';
+import {
+    ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+  } from 'recharts';
 class App extends Component {
   state = {
     events: [],
@@ -71,43 +76,60 @@ class App extends Component {
     });
   };
 
-  render() {
-    const { showWelcomeScreen } = this.state;
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
 
-    if (showWelcomeScreen === undefined) {
-      return <div className="App" />;
-    } else if (showWelcomeScreen === true) {
-      return (
-        <WelcomeScreen
-          showWelcomeScreen={this.state.showWelcomeScreen}
-          getAccessToken={() => {
-            getAccessToken();
-          }}
-        />
-      );
-    } else {
-      return (
-        <div className="App">
-          <div className="offline_alert_container">
-            {!this.state.isOnline && (
-              <OfflineAlert
-                style={{ top: 0 }}
-                text={
-                  "You are offline. An updated list will be loaded when you are back online."
-                }
-              />
-            )}
-          </div>
-          <CitySearch
-            updateEvents={this.updateEvents}
-            locations={this.state.locations}
-          />{" "}
-          <NumberOfEvents updateEvents={this.updateEvents} />
-          <EventList events={this.state.events} />
-        </div>
-      );
+  
+     render() {
+         if (this.state.showWelcomeScreen === undefined) return <div className="App" />
+        
+        return ( 
+            <div className = "App">
+            {!navigator.onLine && <OfflineAlert text={'You are currently offline, data may be not updated.'}/>}
+            <Header / >
+            <Container>
+                    <Row className="d-flex justify-content-center align-item-center p-3 m-3">
+                            <Col md={6} className="d-flex flex-column align-items-center justify-content-center p-5">
+                                    <NumberOfEvents updateEvents = { this.updateEvents } />
+                                    <CitySearch locations = { this.state.locations } updateEvents = { this.updateEvents } />  
+                            </Col>
+                            <Col md={6} className='data-vis-wrapper d-flex flex-column'>
+                                    <EventGenre events={this.state.events} />
+                            </Col>
+                            <Col  md={12} className="d-flex flex-column align-items-center justify-content-around p-5">
+                                    <h6 className="text-muted">Distribution of event amounts by city</h6>
+                                    <ResponsiveContainer height={250} >
+                                        <ScatterChart
+                                                margin={{
+                                                    top: 20, right: 20, bottom: 20, left: 20,
+                                                }}
+                                                >
+                                                <CartesianGrid />
+                                                <XAxis type="category" dataKey="city" name="City" />
+                                                <YAxis type="number" dataKey="number" name="Events" allowDecimals={false} />
+                                                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                                                <Scatter data={this.getData()} fill="rgba(255, 172, 5, 0.671)" />
+                                        </ScatterChart>
+                                    </ResponsiveContainer>
+                                    
+                            </Col>
+                    </Row> 
+                    <Row>
+                    
+                    </Row>
+            <EventList events = { this.state.events } /> 
+            <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
+            </Container > 
+            </div>
+        );
     }
-  }
 }
 
 export default App;
